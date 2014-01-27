9917456a770c83f8b0c27d94fd0d1f0afd43b371
inf106628 <lukasz.grzeszkowiak@student.put.poznan.pl>
2014-01-27 15:52:12 +0100

    Test_client
    Sends hardcoded file.

---
+++ Send_test/Send_test/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Send_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label_ip = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_filename = new System.Windows.Forms.TextBox();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(134, 22);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(100, 20);
+            this.textBox_ip.TabIndex = 0;
+            this.textBox_ip.Text = "localhost";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(134, 61);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(100, 20);
+            this.textBox_port.TabIndex = 1;
+            this.textBox_port.Text = "5656";
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.Location = new System.Drawing.Point(108, 29);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(20, 13);
+            this.label_ip.TabIndex = 2;
+            this.label_ip.Text = "IP:";
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(99, 68);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(29, 13);
+            this.label_port.TabIndex = 3;
+            this.label_port.Text = "Port:";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(134, 127);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(100, 23);
+            this.button_send.TabIndex = 4;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Filename";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Path:";
+            // 
+            // textBox_filename
+            // 
+            this.textBox_filename.Location = new System.Drawing.Point(102, 215);
+            this.textBox_filename.Name = "textBox_filename";
+            this.textBox_filename.Size = new System.Drawing.Size(100, 20);
+            this.textBox_filename.TabIndex = 6;
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Location = new System.Drawing.Point(102, 176);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(100, 20);
+            this.textBox_path.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_filename);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_ip);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_filename;
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Send_test/Send_test/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Send_test
+{
+    public partial class Form1 : Form
+    {
+        private Form obj;
+        private Socket mySock;
+        delegate void setThreadedButtonCallback(bool status);
+        delegate void setThreadedLabelCallback(string napis);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.obj = this;
+            this.mySock = null;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            // Confirm user wants to close
+            switch (MessageBox.Show(this, "Are you sure you want to close?", "Closing", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.No:
+                    e.Cancel = true;
+                    return;
+                case DialogResult.Yes:
+                    if (mySock != null)
+                    {
+                        mySock.Shutdown(SocketShutdown.Both);
+                        mySock.Close();
+                    }
+                    return;
+                default:
+                    return;
+            }
+        }
+
+
+
+        private void setThreadedButton(bool status)
+        {
+            if (this.button_send.InvokeRequired)
+            {
+                setThreadedButtonCallback buttonCallback = new setThreadedButtonCallback(setThreadedButton);
+                this.obj.Invoke(buttonCallback, status);
+            }
+            else
+            {
+                this.button_send.Enabled = status;
+            }
+        }
+
+        private void setThreadedLabel(string napis)
+        {
+            if (this.label3.InvokeRequired)
+            {
+                setThreadedLabelCallback buttonCallback = new setThreadedLabelCallback(setThreadedLabel);
+                this.obj.Invoke(buttonCallback, napis);
+            }
+            else
+            {
+                this.label3.Text = napis;
+            }
+        }
+
+
+
+        // OBSLUGA POLACZENIA (callbacks)
+        //======================================================================
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                /* retrieve the SocketStateObject */
+                SendStateObject state = (SendStateObject)ar.AsyncState;
+                Socket socketFd = state.m_SocketFd;
+
+                /* sent data */
+                int size = socketFd.EndSend(ar);
+                state.m_sent = state.m_sent + size;
+
+                if (state.m_sent < state.m_data_size)
+                {
+                    /* send the rest of the data */
+                    socketFd.BeginSend(state.m_DataBuf, state.m_sent, state.m_data_size-state.m_sent, 0, new AsyncCallback(SendCallback), state);
+                }
+                else
+                {
+                    /* all the data has arrived */
+
+                        setThreadedButton(true);
+
+                    
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                setThreadedButton(true);
+            }
+        }
+
+
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                //otwarcie na sucho wpisanego pliku i zamiana go na tablice byte
+                string fileName = "myzip.zip";
+                //string fileName = this.textBox_filename.Text.ToString();
+                string filePath = "C://Users/Lukashoo/Desktop/Sieci projekt/";
+                //string filePath = this.textBox_path.Text.ToString();
+                byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+                setThreadedLabel(Path.Combine(filePath, fileName));
+                byte[] fileData = File.ReadAllBytes(Path.Combine(filePath, fileName));
+                MessageBox.Show(fileData.Length.ToString());
+                byte[] clientData = new byte[4 + 4 + fileNameByte.Length + fileData.Length];
+                byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+                byte[] fileDataLen = BitConverter.GetBytes(fileData.Length);
+                fileNameLen.CopyTo(clientData, 0);
+                fileDataLen.CopyTo(clientData, 4);
+                fileNameByte.CopyTo(clientData, 8);
+                fileData.CopyTo(clientData, 8 + fileNameByte.Length);
+
+
+
+                /* retrieve the socket from the state object */
+                Socket socketFd = (Socket)ar.AsyncState;
+
+                /* complete the connection */
+                socketFd.EndConnect(ar);
+
+                /* create the SocketStateObject */
+                SendStateObject state = new SendStateObject();
+                state.m_SocketFd = socketFd;
+                state.m_DataBuf = clientData;
+                state.m_data_size = clientData.Length;
+                state.m_sent = 0;
+
+               // byte[] byteData = Encoding.ASCII.GetBytes(this.textBoxAddr.Text.ToString());
+                /* begin sending the data */
+                //socketFd.BeginReceive(state.m_DataBuf, 0, SocketStateObject.BUF_SIZE, 0, new AsyncCallback(ReceiveCallback), state);
+               socketFd.BeginSend(clientData, state.m_sent, clientData.Length-state.m_sent, 0, new AsyncCallback(SendCallback), state);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                setThreadedButton(true);
+            }
+        }
+
+
+        private void GetHostEntryCallback(IAsyncResult ar)
+        {
+            try
+            {
+                IPHostEntry hostEntry = null;
+                IPAddress[] addresses = null;
+                Socket socketFd = null;
+                IPEndPoint endPoint = null;
+
+                /* complete the DNS query */
+                hostEntry = Dns.EndGetHostEntry(ar);
+                addresses = hostEntry.AddressList;
+
+                /* create a socket */
+                socketFd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.mySock = socketFd;
+
+                /* remote endpoint for the socket */
+                endPoint = new IPEndPoint(addresses[1], Int32.Parse(this.textBox_port.Text.ToString()));
+
+
+                /* connect to the server */
+                socketFd.BeginConnect(endPoint, new AsyncCallback(ConnectCallback), socketFd);
+                setThreadedButton(true);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                setThreadedButton(true);
+            }
+        }
+
+        // ================================================================
+        
+        //OBSLUGA EVENTOW
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                setThreadedButton(false);
+
+                if (this.textBox_ip.Text.Length > 0 && this.textBox_port.Text.Length > 0)
+                {
+                    /* get DNS host information */
+                    Dns.BeginGetHostEntry(this.textBox_ip.Text.ToString(), new AsyncCallback(GetHostEntryCallback), null);
+                }
+                else
+                {
+                    if (this.textBox_ip.Text.Length <= 0) MessageBox.Show("No server address!");
+                    else
+                        if (this.textBox_port.Text.Length <= 0) MessageBox.Show("No server port number!");
+                    setThreadedButton(true);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception:\t\n" + exc.Message.ToString());
+                setThreadedButton(true);
+            }
+        }
+
+    }
+
+    public class SendStateObject
+    {
+        public int m_data_size;
+        public int m_sent;
+        public byte[] m_DataBuf;
+        public Socket m_SocketFd = null;
+    }
+
+}
